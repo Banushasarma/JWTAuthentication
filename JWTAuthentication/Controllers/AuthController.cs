798d1cb04a92e715ca1bf3c073a46e8564726b21
Banu Sharma <banusharma03@outlook.com>
2019-09-26 23:34:19 +0530

    Add project files.

---
+++ JWTAuthentication/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("token")]
+        public ActionResult GetToken()
+        {
+            //Security Key
+            string securityKey = "This is my security key";
+
+            //Symmetric security key
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+
+            //signing credientials
+            var signingCredientials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            //create claims
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Role, "Administrator"));
+
+            //create token
+            var token = new JwtSecurityToken(
+                    issuer: "banu.lk",
+                    audience: "readers",
+                    expires: DateTime.Now.AddHours(1),
+                    signingCredentials: signingCredientials,
+                    claims: claims
+                    );
+
+            //return token
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+
+
+        }
+    }
+}
